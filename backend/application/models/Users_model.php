475425d599119eadb08e222a74e4bf9e658bf234
sachin9421002025 <sachin.mhaskar7@gmail.com>
2019-07-21 08:10:07 +0530

    login API

---
+++ backend/application/models/Users_model.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users_model extends CI_Model{
+    
+     function login($username, $password)
+    {
+        $this->db->where('username', $username);
+        $this->db->where('password', $password);
+        $query = $this->db->get('hr_login');
+
+        if($query->num_rows() == 1) {
+            return true;
+        }
+
+        return false;
+                
+    }
+}
+?>
